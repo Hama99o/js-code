1cd00ee86c21771b35c25cf81c519ac72980f40c
Hama <72570079+Hama99o@users.noreply.github.com>
2022-04-03 00:28:21 +0200

    new update

--- object.js
+++ object.js
@@ -1,7 +1,7 @@
 let person = {
     name: 'Hama',
     age: 30
-};
+}
  person.name = "Asad";
- person['name'] = 'Jack';
-console.log(person.name);
+ person['name'] = 'Jack'
+console.log(person.name)
